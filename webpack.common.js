829839ca8be6e950c9f61c6da8d85b5522492472
Expedito Andrade <expeditojazz@gmail.com>
2020-07-06 12:13:12 -0300

    webpack works without merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,25 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  plugins: [new HtmlWebpackPlugin({
-    template: '.src/template.html',
-  })],
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js",
+  },
   module: {
     rules: [
       {
-        test: /\.html$/i,
-        use: ['html-loader'],
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "assets",
+          },
+        },
       },
     ],
   },
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,39 +1,35 @@
-// /* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// const path = require('path');
-// const merge = require('webpack-merge');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const common = require('./webpack.common');
-
-// module.exports = merge(common, {
-//   mode: 'development',
-//   output: {
-//     filename: 'main.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   plugins: [new HtmlWebpackPlugin({
-//     template: './src/template.html',
-//   }),
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.s[ac]ss$/i,
-//         use: [
-//           // Creates `style` nodes from JS strings
-//           {
-//             loader: 'style-loader',
-//           },
-//           // Translates CSS into CommonJS
-//           {
-//             loader: 'css-loader',
-//           },
-//           // Compiles Sass to CSS
-//           {
-//             loader: 'sass-loader',
-//           },
-//         ],
-//       },
-//     ],
-//   },
-// });
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js",
+  },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
