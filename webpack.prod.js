5d3b93a00b3ddcd944d31d20448133d0cc7e5a63
Expedito Andrade <expeditojazz@gmail.com>
2020-07-06 10:45:26 -0300

    initial webpack setup

---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "main.[contentHash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+});
