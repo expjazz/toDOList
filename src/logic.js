3138d20f46d3bb7899947badcd0e25ef225dd43f
mariosknl <mariosknl@outlook.com>
2020-07-08 17:01:22 +0300

    LocalStorage functionality completed

--- src/logic.js
+++ src/logic.js
@@ -7,6 +7,12 @@ import pageGen from './mainFile';
 const projects = JSON.parse(localStorage.getItem('Projects')) || [];
 
 const run = () => {
+  const createToDoList = Project.Project('Today', 'LiveSession', 'Today', 'Important');
+  if (projects.length === 0) projects.push(createToDoList);
+
+
+  // createToDoWebsite.items.push(navbar);
+
   pageGen.mainPage();
   generator.projectGenerator(projects);
 };
